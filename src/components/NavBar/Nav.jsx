8c165f9a249caef72d98a96fdf9f93c7c2c69ebb
Amarah55-coder <MaheryRAZAFIMANANTSOA55@gmail.com>
2025-07-05 09:56:49 +0300

    Fiin du nav bar

---
+++ src/components/NavBar/Nav.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import heiLogo from "../../assets/logo.png";
+import "../../css/Nav.css"; 
+
+export const NavBar = () => {
+  const navLinks = [
+    { name: "Accueil", href: "#accueil" },
+    { name: "Actualité", href: "#actualite" },
+    { name: "Bourse d'étude", href: "#bourse-etude" },
+    { name: "Inscription", href: "#inscription" },
+  ]
+  return (
+    <header className="bg-[#001948] shadow-md">
+      <nav className="container mx-auto px-6 sm:px-10 lg:px-16 py-3 flex items-center justify-between">
+        <a href="/" className="flex-shrink-0">
+          <img src={heiLogo} className="h-12 object-contain" alt="Hei logo" />{" "}
+        </a>
+        <div className="flex items-center space-x-6">
+          <ul className="flex space-x-6 lg:space-x-8">
+            {navLinks.map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.href}
+                  className="text-white hover:text-[#f8bd1a] font-sans uppercase text-sm transition duration-300 ease-in-out font-semibold"
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden md:block">
+            <button className="bg-[#DFA408] hover:bg-[#f8bd1a] text-white font-sans uppercase text-sm px-6 py-2 rounded-xl shadow-lg transition duration-300 ease-in-out transform hover:scale-105 font-semibold">
+              Intranet
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#fff",
+      blue: "#001948",
+      yellow: "#f8bd1a",
+    },
+    extend: {
+      bannerImg: "url('/hei-banner-1.webp')",
+      blackOverlay: "linear-gradien(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 100%)",
+    },
   },
   plugins: [],
 };
